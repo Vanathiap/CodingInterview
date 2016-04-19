deb52b52667a2b1b3e7f9dcbfb14583df179ca51
Vanathi <vanathiap@github.com>
2016-04-19 11:55:49 -0400

    BST

---
+++ HelloWorld/Trees/BST.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Trees
+{
+	public class BSNode
+	{
+		int data;
+		BSNode left;
+		BSNode right;
+
+		public BSNode ()
+		{
+			
+		}
+
+		public BSNode (int data, BSNode left, BSNode right)
+		{
+			this.data = data;
+			this.left = left;
+			this.right = right;
+		}
+
+		public int Data {
+			get { return data;}
+			set { data = value; }
+		}
+
+		public BSNode Left {
+			get { return left;}
+			set { left = value; }
+		}
+
+		public BSNode Right {
+			get { return right;}
+			set { left = value; }
+		}
+	}
+
+	public static class BST
+	{
+		public static BSNode Search(int data, BSNode node)
+		{
+			if (node == null)
+				return null;
+			else if (node.Data == data)
+				return node;
+			else if (node.Data < data)
+				return Search (data, node.Right);
+			else
+				return Search (data, node.Left);
+		}
+
+		public static BSNode Create(int[] sortedArray, int startIndex, int endIndex)
+		{
+			if (startIndex > endIndex)
+				return null;
+			else {
+				var rootNode = new BSNode ();
+				int midIndex = (endIndex + startIndex) / 2;
+				rootNode.Data = sortedArray [midIndex];
+				rootNode.Left = Create (sortedArray, startIndex, midIndex - 1);
+				rootNode.Right = Create (sortedArray, midIndex + 1, endIndex);
+				return rootNode;
+			}
+		}
+	}
+}
---
+++ HelloWorld/Trees/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Trees
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			var sortedArray = new int[] { 1, 2, 3, 4, 5, 6 };
+			var root = BST.Create (sortedArray, 0, sortedArray.Length - 1);
+
+			var node = BST.Search (10, root);
+			if (node == null)
+				Console.WriteLine ("10 not found");
+			else
+				Console.WriteLine ("Found, 10->Left {0}, Right= {1}", node.Left == null ? "null" : node.Left.Data.ToString(), node.Right == null ? "null" : node.Right.Data.ToString());
+
+			node = BST.Search (4, root);
+			if (node == null)
+				Console.WriteLine ("4 not found");
+			else
+				Console.WriteLine ("Found, 4->Left {0}, Right= {1}", node.Left == null ? "null" : node.Left.Data.ToString(), node.Right == null ? "null" : node.Right.Data.ToString());
+		}
+	}
+}
